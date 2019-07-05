d1d8ea6f506b8f88022f3a0becbfff0f7ec8e92c
Nicholaus Rowe <nrowe@DumpsterFire.local>
2019-07-04 18:10:53 -0700

    Flipping One Card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+$(document).ready(initializeApp);
+function initializeApp() {
+    $(".card").on("click", ".card-back", handleCardClick);
+}
 
+
+function handleCardClick(event) {
+    var clickedCard = ($(event.currentTarget));
+    clickedCard.addClass('hidden');
+}
