2808caa081983bec887428516c0d7b8bad97d064
Nicholaus Rowe <nrowe@DumpsterFire.local>
2019-07-06 01:41:09 -0700

    match 2 cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,53 +8,40 @@ var secondCardClicked = null;
 var matches = null;
 
 
-
-
-//var firstCardSource = $(this).find('.back').css(background - image);
-// var secondCardSource = $("secondCardClicked.find('front')").css("background-image");
-
 function handleCardClick(event) {
-    var clickedCard = $(event.currentTarget).find('.card-back');
+    var clickedCard = $(event.currentTarget).find('.back');
     clickedCard.addClass('hidden');
+
     if (firstCardClicked == null) {
         firstCardClicked = $(event.currentTarget);
-        console.log(firstCardClicked);
-        var firstCardSource = $(firstCardClicked).find('.front').css('background-image');
-        console.log(firstCardSource);
     } else {
         secondCardClicked = $(event.currentTarget);
-        console.log(secondCardClicked);
+
+        var firstCardSource = $(firstCardClicked).find('.front').css('background-image');
+        console.log(firstCardSource);
+
         var secondCardSource = $(secondCardClicked).find('.front').css('background-image');
         console.log(secondCardSource);
+
         if (firstCardSource === secondCardSource) {
             console.log("MATCH");
+            matches++
+            console.log(matches);
         } else {
             console.log("NO MATCH");
+            var unhide1 = $(firstCardClicked).find('.back')
+            setTimeout(function () { (unhide1).removeClass("hidden");}, 1500);
+            var unhide2 = $(secondCardClicked).find('.back')
+            setTimeout(function () { (unhide2).removeClass("hidden"); }, 1500);
         }
     }
 }
-// var target = $(this);
-// var backDom = target.find('.back');
 
-// $(this).find('.back').css(background - image)
+// $(selector).removeClass(classname)
 
+// setTimeout(function () { alert("Hello"); }, 3000);
 
-// var firstDiv = firstCardClicked.find('front')
-// console.log(firstDiv)
-
-// var firstCardSource =
-
-// $(thing).css("background-image");
-
-// console.log(firstCardSource)
-
-// $("ul li:nth-child(2)").append("<span> - 2nd!</span>");
-
-//firstCardClicked.find('front')
-
-
-
-
-
-
-//var firstCardClicked = $(event.currentTarget)
+// function unhide() {
+//     var element = document.getElementById("myDIV");
+//     element.classList.remove("mystyle");
+// }
