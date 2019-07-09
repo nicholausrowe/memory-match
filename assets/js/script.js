53c3d7be25673fde1bbcfef0af606024ca971c64
Nicholaus Rowe <nrowe@DumpsterFire.local>
2019-07-09 10:45:33 -0700

    Tracking Stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,53 @@ function toggleModal() {
 
 var firstCardClicked = null;
 var secondCardClicked = null;
-var matches = null;
-var max_matches = 2;
+var matches = 0;
+var max_matches = 9;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
+var currentlyFlipped = null;
+
+var displayStats;
+
+function calculateAccuracy() {
+    accuracy = matches / attempts * 100
+}
+
+function displayStats() {
+    calculateAccuracy();
+    $('.attemptsNum').text(attempts);
+    $('.accuracyPerc').text(accuracy.toFixed(0) + "%");
+    $('.gamesNum').text(games_played);
+}
+
 
 
 function handleCardClick(event) {
+    // debugger;
+    if (currentlyFlipped !== 2){
     var clickedCard = $(event.currentTarget).find('.back');
-    clickedCard.addClass('hidden');
+    }
 
-    if (firstCardClicked == null) {
+    if (firstCardClicked == null && clickedCard.hasClass("hidden") === false) {
         firstCardClicked = $(event.currentTarget);
+        clickedCard.addClass('hidden');
+        currentlyFlipped += 1;
     } else {
+        if (clickedCard.hasClass("hidden") === false){
         secondCardClicked = $(event.currentTarget);
+        clickedCard.addClass('hidden');
+        currentlyFlipped += 1;
+
+        if (secondCardClicked != null) {
+            debugger;
+            attempts++;
+
+            // $('.accuracyPerc').text(accuracy.toFixed(2) + "%");
+            // $('.attemptsNum').text(attempts);
+
+        }
+
 
         var firstCardSource = $(firstCardClicked).find('.front').css('background-image');
         console.log(firstCardSource);
@@ -30,22 +65,32 @@ function handleCardClick(event) {
         if (firstCardSource === secondCardSource) {
             console.log("MATCH");
             matches++
+            displayStats();
             console.log(matches);
+            currentlyFlipped = 0;
             firstCardClicked = null;
             secondCardClicked = null;
         } else {
             console.log("NO MATCH");
+            displayStats();
             var unhide = $(firstCardClicked).add(secondCardClicked).find('.back')
-            setTimeout(function () { (unhide).removeClass("hidden");}, 1500);
+            setTimeout(function () {
+                currentlyFlipped = 0;
+                (unhide).removeClass("hidden");}, 1500);
 
             firstCardClicked = null;
             secondCardClicked = null;
+
         }
     }
+}
     if (matches === max_matches) {
+        games_played++;
+        displayStats();
+        // $('.gamesNum').text(games_played);
         $('.modal-shadow').toggleClass('hidden');
         $('.modal').append('<button class="button">OH SHIT SON<br>YOU WON!</button>');
-        $('.button').addEventListener("click", killModal);
+        $('.button').addEventListener("click", toggleModal);
 
     }
 
